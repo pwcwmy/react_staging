9c86c9fe846ff0f5e289e648efb349d071f680e6
潘玮成 <pwc@panweichengdeMacBook-Air.local>
2025-01-02 23:10:57 +0800

    菜单点击跳转和用户form实现

--- 04_src_配置代理/index.js
+++ 04_src_配置代理/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client';  // 后面/client一定要加
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
